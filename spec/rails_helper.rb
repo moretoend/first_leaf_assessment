e86278a240375358c02867b125956beb129a33fa
Daniel Moreto <dfmoreto@gmail.com>
2024-06-02 16:13:24 -0300

    chore: start Simplecov on test env

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+
 require 'rspec/rails'
+require 'simplecov'
 
 Rails.root.glob('spec/support/**/*.rb').each { |f| require f }
 
@@ -21,3 +23,5 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 end
+
+SimpleCov.start
