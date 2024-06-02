6937679755497b913b471c9d0a56be970e588291
Daniel Moreto <dfmoreto@gmail.com>
2024-06-02 16:16:20 -0300

    feat: configure error when unpermitted param error is raised

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  rescue_from ActionController::UnpermittedParameters, with: :render_params_error
+
+  private
+
+  def render_params_error(exception)
+    @message = "Unpermitted parameters were sent: #{exception.params.join(", ")}"
+    render 'shared/error', status: :unprocessable_entity
+  end
 end
