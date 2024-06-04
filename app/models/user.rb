4ec84b4be7309ceec1455d22bac7caa1fe68e0b9
Daniel Moreto <dfmoreto@gmail.com>
2024-06-04 08:38:09 -0300

    feat: update model User it was updated to autogenerate :key before_validation, to set :key as read only as to keep only password length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,21 @@
 class User < ApplicationRecord
+  KEY_SIZE = 100
+  private_constant :KEY_SIZE
+
   has_secure_password
+
+  attr_readonly :key
 
   validates :email, presence: true, length: { maximum: 200 }, uniqueness: { case_sensitive: false }
   validates :phone_number, presence: true, length: { maximum: 20 }, uniqueness: { case_sensitive: false }
   validates :full_name, length: { maximum: 200 }
-  validates :password, presence: true, length: { maximum: 60 }
-  validates :key, presence: true, length: { maximum: 100 }, uniqueness: true
+  validates :password, length: { maximum: 60 }
   validates :account_key, length: { maximum: 100 }, uniqueness: true
   validates :metadata, length: { maximum: 2000 }
+
+  before_validation :generate_key, on: :create
+
+  def generate_key
+    self.key = SecureRandom.alphanumeric(KEY_SIZE)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 describe User do
-  subject { build(:user) }
+  subject { build(:user, key: '1234') }
 
   it { is_expected.to have_secure_password }
+
+  it { is_expected.to have_readonly_attribute(:key) }
 
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_length_of(:email).is_at_most(200) }
@@ -17,12 +19,13 @@ describe User do
 
   it { is_expected.to validate_length_of(:password).is_at_most(60) }
 
-  it { is_expected.to validate_presence_of(:key) }
-  it { is_expected.to validate_length_of(:key).is_at_most(100) }
-  it { is_expected.to validate_uniqueness_of(:key) }
-
   it { is_expected.to validate_length_of(:account_key).is_at_most(100) }
   it { is_expected.to validate_uniqueness_of(:account_key) }
 
   it { is_expected.to validate_length_of(:metadata).is_at_most(2000) }
+
+  it 'generates a key with 100 characters before validation' do
+    subject.validate
+    expect(subject.key.size).to eq 100
+  end
 end
