d752c97f438d51b742cf2dd99c7f02bb72a83e67
Daniel Moreto <dfmoreto@gmail.com>
2024-06-04 11:27:42 -0300

    feat: set a default error recovering to render :internal_server_error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  rescue_from StandardError, with: :render_internal_server_error
   rescue_from ActionController::UnpermittedParameters, with: :render_params_error
 
   private
@@ -6,5 +7,11 @@ class ApplicationController < ActionController::API
   def render_params_error(exception)
     @errors = ["Unpermitted parameters were sent: #{exception.params.join(", ")}"]
     render 'shared/error', status: :unprocessable_entity
+  end
+
+  def render_internal_server_error(exception)
+    Rails.logger.error(exception.backtrace.join("\n"))
+    @errors = ["Something bad happened on our side"]
+    render 'shared/error', status: :internal_server_error
   end
 end
