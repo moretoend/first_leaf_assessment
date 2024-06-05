acbd903669be8d40d4e65fa774cb8edb8d54cf8d
Daniel Moreto <dfmoreto@gmail.com>
2024-06-04 22:01:20 -0300

    test: update users factory it was updated to remove :account_key to be generated

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,6 @@ FactoryBot.define do
     phone_number { Faker::PhoneNumber.phone_number }
     full_name { Faker::Name.name }
     password { Faker::Internet.password(max_length: 60, special_characters: true) }
-    account_key { SecureRandom.alphanumeric(100) }
     metadata { "#{Faker::Gender.type}, age #{SecureRandom.rand(70)}, #{Faker::Job.title}, #{Faker::Job.position}" }
   end
 end
